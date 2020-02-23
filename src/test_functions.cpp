1440705d070ed686a6fac47600603e206bedf530
Erick Velez <erickvelez7@gmail.com>
2020-02-23 10:58:05 -0800

    test_functions: use unique_ptrs instead of raw ptrs

--- src/test_functions.cpp
+++ src/test_functions.cpp
@@ -3,20 +3,39 @@
 
 #define SET_LIMIT 8
 
-void fill_array(double*, double*, std::set<std::tuple<double, double, double>> const*);
+void fill_array(double*, std::unique_ptr<double[]>&, std::unique_ptr<std::set<std::tuple<double, double, double>>> const&);
 
+/**
+ * f(x,y,z) = x^2 + y^2 + z^2
+ *
+ * @param x
+ * @param y
+ * @param z
+ * @return the value of f(x) @ (x,y,z) of type double
+ */
 double function_1(double x, double y, double z) {
     return (std::pow(x, 2) + std::pow(y, 2) + std::pow(z, 2));
 }
 
+/**
+ * f(x,y,z) = sin(x+y+z)
+ *
+ * @param x
+ * @param y
+ * @param z
+ * @return the value of f(x) @ (x,y,z) of type double
+ */
 double function_2(double x, double y, double z) {
     return sin(x+y+z);
 }
 
 /**
- * f(x,y,z)=x^2 + y^2 + z^2
+ * Uses function_1 as the function to approximate through tricubic interpolation
+ *
+ * @param test_points 
+ * @return unique_ptr<double[]> an array of approximations of the same size as test_points
  */
-double* test_function_1(std::set<std::tuple<double, double, double>> const* test_points) {
+std::unique_ptr<double[]> test_function_1(std::unique_ptr<std::set<std::tuple<double, double, double>>> const& test_points) {
     double f[8] = {0, 1, 1, 2, 1, 2, 2, 3};
     double df_dx[8] = {0, 2, 0, 2, 0, 2, 0, 2};
     double df_dy[8] = {0, 0, 2, 2, 0, 0, 2, 2};
@@ -27,7 +46,7 @@ double* test_function_1(std::set<std::tuple<double, double, double>> const* test
     double d3f_dxdydz[8] = {0, 0, 0, 0, 0, 0, 0};
 
     double alpha[64];
-    auto* approximations = new double[test_points->size()];
+    std::unique_ptr<double[]> approximations(new double[test_points->size()]);
 
     tricubic_get_coeff(alpha, f, df_dx, df_dy, df_dz, d2f_dxdy, d2f_dxdz, d2f_dydz, d3f_dxdydz);
 
@@ -39,7 +58,7 @@ double* test_function_1(std::set<std::tuple<double, double, double>> const* test
 /**
  * f(x,y,z)=sin(x+y+z)
  */
-double* test_function_2(std::set<std::tuple<double, double, double>> const* test_coords) {
+std::unique_ptr<double[]> test_function_2(std::unique_ptr<std::set<std::tuple<double, double, double>>> const& test_coords) {
     double f[8];
     double df_dx[8];
     double df_dy[8];
@@ -74,7 +93,7 @@ double* test_function_2(std::set<std::tuple<double, double, double>> const* test
     }
 
     tricubic_get_coeff(alpha, f, df_dx, df_dy, df_dz, d2f_dxdy, d2f_dxdz, d2f_dydz,d3f_dxdydz);
-    auto* approximations = new double[test_coords->size()];
+    std::unique_ptr<double[]> approximations(new double[test_coords->size()]);
 
     fill_array(alpha, approximations, test_coords);
     return approximations;
@@ -86,13 +105,13 @@ double* test_function_2(std::set<std::tuple<double, double, double>> const* test
  * @param approximations
  * @param test_coords
  */
-void fill_array(double* alpha, double* approximations, std::set<std::tuple<double, double, double>> const* test_coords) {
+void fill_array(double* alpha, std::unique_ptr<double[]>& approximations, std::unique_ptr<std::set<std::tuple<double, double, double>>> const& test_points) {
     int i = 0;
-    for (auto coord : *test_coords) {
+    for (auto coord : *test_points) {
         double x = std::get<0>(coord);
         double y = std::get<1>(coord);
         double z = std::get<2>(coord);
 
-        *(approximations + i) = tricubic_eval(alpha, x, y, z);
+        approximations[i] = tricubic_eval(alpha, x, y, z);
     }
 }
--- src/test_functions.h
+++ src/test_functions.h
@@ -5,7 +5,9 @@
 #include <set>
 #include <tuple>
 
-double* test_function_1(std::set<std::tuple<double, double, double>> const*);
-double* test_function_2(std::set<std::tuple<double, double, double>> const*);
+double function_1(double, double, double);
+double function_2(double, double, double);
+std::unique_ptr<double[]> test_function_1(std::unique_ptr<std::set<std::tuple<double, double, double>>> const&);
+std::unique_ptr<double[]> test_function_2(std::unique_ptr<std::set<std::tuple<double, double, double>>> const&);
 
 #endif //TRIC_EXAMPLE_TEST_FUNCTIONS_H
